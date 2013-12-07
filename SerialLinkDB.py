2797263b8d42364a7dd2e510b4f5b1e037d9dbfe
Oscar Robinson <oscarrobinson@btinternet.com>
2013-12-07 21:53:16 +0000

    added code to extract id, timestamp and data from string

--- SerialLinkDB.py
+++ SerialLinkDB.py
@@ -3,10 +3,17 @@ import time
 import string
 import random
 import serial
- 
+def splitString(str):
+	result = []
+	for s in str.split():
+		if s.isdigit():
+			result.append(int(s))
+	return result
+
+
 # configure the serial connections
 ser = serial.Serial(
-	port='/dev/ttyUSB1',
+	port='/dev/cu.usbserial-FTWVXL40B',
 	baudrate=115200,
 	parity=serial.PARITY_NONE,
 	stopbits=serial.STOPBITS_ONE,
@@ -27,18 +34,36 @@ db = MySQLdb.connect(host="eu-cdbr-azure-west-b.cloudapp.net",
 cur = db.cursor() 
 cur.execute("TRUNCATE TABLE lightsensor;")
 
-#count = 1;
+count = 0;
+startTimestampRead = 0
+startTimestamp = 0
+
 while (True):
 	if (ser.inWaiting() > 0):
 		print "reading data"
-		serdata = float(ser.readline())
-		print serdata
-		num = time.time()
-		query="INSERT INTO lightsensor(timestamp,data)VALUES("+str(num)+",'"+str(serdata)+"');"
-		print query
-		cur.execute(query)
-		db.commit()
-		print "time " + str(num) 
+		serdata = ser.readline()
+		numbers = splitString(serdata)
+		hardwareId = numbers[0]
+		data = numbers[1]
+		timestamp = numbers[2]
+
+		#if first reading, save current time and first timestamp read, can then calculate time of reading for all other readings
+		if count==0:
+			startTimestampRead = int(timestamp)
+			startTimestamp = int(time.time())
+			timestamp = startTimestamp
+		#calculate the time the reading was made
+		else:
+			timestamp = startTimestamp + (timestamp-startTimestampRead)
+
+		print "ID: "+str(hardwareId)+"|| reading: "+str(data)+" || timestamp: "+str(timestamp)
+		
+		count+=1
+
+		#query="INSERT INTO lightsensor(timestamp,data)VALUES("+str(num)+",'"+str(serdata)+"');"
+		#print query
+		#cur.execute(query)
+		#db.commit()
 		#count = count+1
 
 ser.close()
