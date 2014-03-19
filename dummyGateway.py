ccf3976c901ac21efd5eb06798fe0ebc97fab116
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-19 15:01:59 +0000

    added dummy gateway script for use with new database

---
+++ dummyGateway.py
@@ -0,0 +1,91 @@
+import MySQLdb
+import time
+import string
+import random
+
+piId = "00000000da3e31f0"
+sensor1ID = "ab0f12e4ef0102f7"
+sensor2ID = "ab0f12e4efa5a269"
+sensor3ID = "ab0f12e4ef1214f7"
+sensor1ont = "1"
+sensor2ont = "1"
+sensor3ont = "2"
+sensor1data = 20
+sensor2data = 20
+sensor3data = 23
+
+
+
+
+#create database object and connect
+db = MySQLdb.connect(host="eu-cdbr-azure-north-b.cloudapp.net", 
+                     user="b56834bde0c85e",
+                      passwd="87a230d7",
+                      db="makesensemain")
+ 
+
+cur = db.cursor() 
+
+sensor_ids = []
+
+counter = 0
+
+netId = ""
+sensorId = ""
+sensorOnt = ""
+sensorData = 0
+sensorTime = 0
+
+while (True):
+
+		counter+=1
+
+		if counter == 1:
+			netId = piId
+			sensorId = sensor1ID
+			sensorOnt = sensor1ont
+			sensorData = sensor1data
+			sensor1data = random.randint(10,1000)
+
+		elif counter == 2:
+			netId = piId
+			sensorId = sensor2ID
+			sensorOnt = sensor2ont
+			sensorData = sensor2data
+			sensor2data = random.randint(10,1000)
+
+		elif counter == 3:
+			netId = piId
+			sensorId = sensor3ID
+			sensorOnt = sensor3ont
+			sensorData = sensor3data
+			sensor3data = random.randint(10,25)
+			counter = 0
+
+		sensorTime = time.time()
+
+
+		if sensorId not in sensor_ids:
+			sensor_ids.append(sensorId)
+			query = "SELECT COUNT(1) FROM sensors WHERE sensor_id=\'"+sensorId+"\'"
+			cur.execute(query)
+			db.commit()
+			if cur.fetchone()[0]==0:
+				print "Adding "+sensorId+" to sensors"
+				query = "INSERT INTO sensors(sensor_id, network_id, ontology_id) VALUES(\'"+sensorId+"\',\'"+netId+"\',\'"+sensorOnt+"\')"
+				cur.execute(query)
+				db.commit()
+
+		print "id: "+sensorId+" || timestamp: "+str(sensorTime)+" || data: "+str(sensorData)
+
+		query = "INSERT INTO data(sensor_id, timestamp, reading) VALUES(\'"+sensorId+"\',\'"+str(sensorTime)+"\',\'"+str(sensorData)+"\')"
+		cur.execute(query)
+		db.commit()
+
+
+
+		time.sleep(1)
+
+exit()
+
+
